86ed4116670462825dcaed3710ee8c2bdbc86cac
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-21 22:06:16 +0200

    Add initial files

---
+++ Popup/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Popup;
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MageMastery_Popup', __DIR__);
