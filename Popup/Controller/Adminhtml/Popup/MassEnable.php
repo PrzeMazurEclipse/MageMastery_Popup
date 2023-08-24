708942bb4e30a012e03c2ed26fe4aca50db56a0f
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-24 21:28:40 +0200

    Add mass enable action

---
+++ Popup/Controller/Adminhtml/Popup/MassEnable.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Popup\Controller\Adminhtml\Popup;
+
+use MageMastery\Popup\Api\Data\PopupInterface;
+use MageMastery\Popup\Api\PopupRepositoryInterface;
+use MageMastery\Popup\Model\Popup;
+use MageMastery\Popup\Model\ResourceModel\Popup\CollectionFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Ui\Component\MassAction\Filter;
+
+class MassEnable extends Action
+{
+    public function __construct(
+        Context $context,
+        private readonly Filter $filter,
+        private readonly CollectionFactory $collectionFactory,
+        private readonly PopupRepositoryInterface $popupRepository
+    )
+    {
+        parent::__construct($context);
+    }
+
+    public function execute(): ResultInterface
+    {
+        try{
+            $collection = $this->filter->getCOllection($this->collectionFactory->create());
+            $collectionSize = $collection->getSize();
+
+            /** @var Popup $popup */
+            foreach($collection as $popup) {
+                $popup->setIsActive(PopupInterface::STATUS_ENABLED);
+                $this->popupRepository->save($popup);
+            }
+
+            $this->messageManager->addSuccessMessage(__('A total of %1 record(s) have been enabled', $collectionSize));
+        } catch (\Throwable $exception) {
+            $this->messageManager->addErrorMessage('Something went wrong while processing th operation');
+        }
+
+        $result = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        return $result->setPath('magemastery_popup/popup/index');
+    }
+}
