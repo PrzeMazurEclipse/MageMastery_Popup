33e521893d17cdddf0ed5d63dd2b5808ff889fcd
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-29 09:18:51 +0200

    Add edit and new action controller

---
+++ Popup/Controller/Adminhtml/Popup/NewAction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Popup\Controller\Adminhtml\Popup;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class NewAction extends Action
+{
+    public function execute(): ResultInterface
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_FORWARD)->forward('edit');
+    }
+}
