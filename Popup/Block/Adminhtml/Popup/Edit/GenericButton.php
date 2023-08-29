f25b5493af03ed6009484c8cf08a50d3214309e5
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-29 21:34:25 +0200

    Buttons logic

---
+++ Popup/Block/Adminhtml/Popup/Edit/GenericButton.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Popup\Block\Adminhtml\Popup\Edit;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
+
+class GenericButton
+{
+    /**
+     * @param UrlInterface $url
+     * @param RequestInterface $request
+     */
+    public function __construct(
+        private readonly UrlInterface $url,
+        private readonly RequestInterface $request,
+    ) {
+    }
+
+    /**
+     * @return int
+     */
+    public function getPopupId(): int
+    {
+        return (int) $this->request->getParam('popup_id', 0);
+    }
+
+    /**
+     * Generate url by route and parameters
+     *
+     * @param   string $route
+     * @param   array $params
+     * @return  string
+     */
+    public function getUrl(string $route = '', array $params = []): string
+    {
+        return $this->url->getUrl($route, $params);
+    }
+}
