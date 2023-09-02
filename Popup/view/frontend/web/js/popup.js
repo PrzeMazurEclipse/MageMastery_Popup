cb025a24cac6ab22d371cd6a38ffd37dca9d2b1d
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-09-02 21:29:56 +0200

    Display a popup on the frontend

---
+++ Popup/view/frontend/web/js/popup.js
@@ -0,0 +1,18 @@
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function ($, modal) {
+    'use strict';
+
+    return function (settings) {
+        const content = settings.content,
+            timeout = settings.timeout;
+        const options = {
+            type: 'popup',
+            responsive: true,
+            autoOpen: true,
+        };
+
+        $('<div />').html(content).modal(options);
+    }
+})
